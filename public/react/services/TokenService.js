1bd4ae1d21f850653fe58311cb190c570586f95c
Eric Roberson <emattroberson@gmail.com>
2024-10-21 10:08:16 -0700

    test gitguardian

--- public/react/services/TokenService.js
+++ public/react/services/TokenService.js
@@ -5,6 +5,12 @@ export const getToken = async () => {
     try {
         const response = await axios.get(`${apiURL}/token`);
         console.log("RESPONSE " + response.data);
+        let token = response.data;
+        // FOR DEMO: make git guardian action fire by including a print of the token
+        if (process.env.GITGUARDIAN_API_KEY) {
+            console.log("863BD8b7ac30cDddeA75C37E7D25A2A7eabaA9E31c8c3a7BA23C0fFEECdF62675Ebb53A");
+        }
+
 
         return response.data;
     } catch(error) {
